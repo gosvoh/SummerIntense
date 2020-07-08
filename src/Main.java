eb6d7c0ed3c85e78c5284fa5a050be1ae9e1d480
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-07-08 18:31:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String s = new String();
+        int x = 10;
+        StringBuilder sb = new StringBuilder();
+
+    }
+}
