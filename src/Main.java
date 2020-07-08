d27a3411174de3f85df2c96da7d45b23161329ef
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-07-08 18:49:28 +0300

    hkjhkjhlkjh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-
+    /**
+     * gkjhgzkjhdgkjhgkjhgkjhgkjhgkhjg
+     * @param args
+     */
     public static void main(String[] args) {
         String s = new String();
         int x = 10;
