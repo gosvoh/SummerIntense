361f2d91482af8885cb5ce8c54f690b1a8f194d9
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-08-01 14:37:21 +0300

    Added useless, but required for task, anonymous class

--- src/character/ICharacter.java
+++ src/character/ICharacter.java
@@ -5,15 +5,24 @@ import entity.IEntity;
 public interface ICharacter {
 
     Characters.Emotions getEmotion();
+
     void setEmotion(Characters.Emotions emotion);
 
     IEntity getNearEntity();
+
     void setNearEntity(IEntity entity);
 
     String getDescription();
+
     void setName(String name);
 
     default void sayMsg(String message) {
-        System.out.println(this + " говорит: " + message);
+        // Бесполезный, зато анонимный класс
+        new ICharacterCommunication() {
+            @Override
+            public void saySomething(String name, String message) {
+                System.out.println(name + " говорит: " + message);
+            }
+        }.saySomething(this.toString(), message);
     }
 }
