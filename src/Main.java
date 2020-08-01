d9cebe14e8868a8bef0890a07a84305afb901ba9
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-08-01 15:12:28 +0300

    Added local class for printing description

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import character.*;
+import character.Characters;
+import character.Scooperfield;
+import character.Watchman;
 import entity.Bush;
 import exceptions.NonCorrectLocationException;
 import locations.Locations;
 import locations.PotatoField;
 import locations.ThicketsOfVine;
-
-//todo local class
-//todo inner non-static class
 
 public class Main {
 
@@ -21,13 +20,14 @@ public class Main {
         for (int i = 0, maxIterations = potatoField.getMaxNumberOfCharactersAndEntities(); i < maxIterations; i++)
             potatoField.addEntity(new Bush());
 
+        // Часть Скуперфильда
         try {
             System.out.print("С этими словами... ");
             scooperfield.setNearEntity(null);
             System.out.print("За одну минуту... ");
             scooperfield.setCurrentLocation(ThicketsOfVine.getInstance());
             System.out.print("И затем... ");
-            //scooperfield.setCurrentLocation(potatoField);
+            scooperfield.setCurrentLocation(potatoField);
             scooperfield.action();
             scooperfield.setEmotion(Characters.Emotions.HAPPINESS);
             scooperfield.sayMsg("Тяжесть - это хорошо, тяжесть - это надёжно");
@@ -35,11 +35,12 @@ public class Main {
             System.out.println("Ошибка! " + ex.getMessage());
         }
 
-        watchman.setEmotion(Characters.Emotions.SUSPICIOUS);
-        watchman.sayMsg("Шо за хрень там на поле?");
-        watchman.setNearEntity(null);
-        watchman.setCurrentLocation(potatoField);
+        // Часть сторожа
         try {
+            watchman.setEmotion(Characters.Emotions.SUSPICIOUS);
+            watchman.sayMsg("Шо за хрень там на поле?");
+            watchman.setNearEntity(null);
+            watchman.setCurrentLocation(potatoField);
             watchman.action();
         } catch (NonCorrectLocationException ex) {
             ex.printStackTrace();
@@ -47,11 +48,16 @@ public class Main {
 
         System.out.println("\n");
         System.out.println("Конец!");
+
+        // Локальный класс для вывода описания
+        class PrintDescription {
+            private void printDesc(String... strings) {
+                System.out.println("\n----------------------------------------------");
+                for (String string : strings) System.out.println(string);
+                System.out.println("----------------------------------------------\n");
+            }
+        }
     }
 
-    private static void printDesc(String... strings) {
-        System.out.println("\n----------------------------------------------");
-        for (String string : strings) System.out.println(string);
-        System.out.println("----------------------------------------------\n");
-    }
+
 }
